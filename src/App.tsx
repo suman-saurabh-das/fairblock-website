13eefd3424e3ee559175e32c23951b1ce3b4e42c
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-01-17 22:00:19 +0530

    Added light/dark mode using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  // useEffect() to set root element background color.
+  useEffect(() => {
+    darkMode
+      ? document.documentElement.style.setProperty("--background-color", "#222")
+      : document.documentElement.style.setProperty(
+          "--background-color",
+          "#fff"
+        );
+  }, [darkMode]);
 
   return (
-    <h1 className="font-marlide font-semibold mt-12 text-center text-3xl">Fairblock Website</h1>
-  )
+    <div
+      className={`App-wrapper text-sm sm:text-base max-w-screen-2xl mx-auto ${
+        darkMode ? "dark" : ""
+      }`}
+    >
+      <div className="App text-black dark:text-white">
+        <h1 className="font-marlide font-semibold mt-12 text-center text-3xl">
+          Fairblock Website
+        </h1>
+        <div className="flex justify-center mt-8">
+          <button
+            className="border px-3 py-1 rounded-xl"
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            Toggle Mode
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
