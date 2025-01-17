6d3f8e32dfe8872613365c2cc705798e466ba605
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-01-17 21:37:44 +0530

    Added custom font marlide using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className="font-semibold mt-12 text-center text-3xl">Fairblock Website</h1>
+    <h1 className="font-marlide font-semibold mt-12 text-center text-3xl">Fairblock Website</h1>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Adding custom font MarlideDisplay
+        marlide: ['"Marlide Display"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
