163211df778ba94a9ac117bf4c26ecc582e1f2e7
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-01-23 23:56:36 +0530

    Updated Backers component to display names in boxes

--- src/components/Backers.tsx
+++ src/components/Backers.tsx
@@ -13,12 +13,12 @@ const backersArray: string[] = [
 const Backers = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <h2 className="font-marlide font-normal mb-4 md:mb-6 text-4xl lg:text-5xl">
+      <h2 className="font-marlide font-normal mb-4 md:mb-12 text-4xl lg:text-5xl">
         Backers
       </h2>
-      <div>
+      <div className="flex flex-wrap gap-2 items-center justify-center">
         {backersArray.map((item, index) => (
-          <span key={index}>{item}, </span>
+          <span className="border border-black dark:border-white font-marlide font-normal px-4 py-1 rounded-xl text-2xl" key={index}>{item}</span>
         ))}
       </div>
     </div>
