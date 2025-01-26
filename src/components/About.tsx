eac43ecd868045e99cac03ef73fedabdab3d6829
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-01-26 17:27:17 +0530

    Updated About - Changed orientation, text size, image size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,40 +1,42 @@
-import { Link } from "react-router-dom";
-
 const About = () => {
   return (
-    <div id="about" className="px-6 py-12 md:py-20">
-      <div className="mx-auto max-w-screen-lg">
-      <h2 className="font-marlide font-normal mb-12 text-4xl md:text-5xl text-center md:text-left">
-        Unlock Credible Economies
-      </h2>
-      <div className="flex flex-col gap-12 md:flex-row-reverse items-start justify-center mx-auto">
-        <div className="flex flex-col gap-12 mx-auto text-xl md:w-1/2">
+    <div id="about" className="pt-32 md:pt-32 px-6 py-12 md:py-20">
+      <div className="flex flex-col lg:flex-row-reverse gap-12 items-center justify-center mx-auto max-w-screen-xl">
+
+        {/* ABOUT IMAGE CONTAINER */}
+        <div className="flex flex-col gap-12 p-4 text-xl mx-auto w-full md:w-1/2 lg:w-[35%]">
           <img
-            className="dark:invert max-w-80 mx-auto w-full"
-            src="/src/assets/images/FairyRing.png"
-            alt=""
+            className="dark:invert mx-auto w-full"
+            src="/src/assets/images/Fairblock.png"
+            alt="Fairblock"
           />
         </div>
-        <div className="md:w-1/2">
-          <p className="md:text-lg mb-6">
+
+        {/* ABOUT TEXT CONTAINER */}
+        <div className="w-full lg:w-3/5">
+          <h2 className="font-marlide font-normal mb-6 text-4xl md:text-5xl">
+            Unlock Credible Economies
+          </h2>
+          <p className="mb-6 md:text-xl">
             Most Web3 applications still depend on centralized intermediaries,
             making protocols less secure and vulnerable to manipulation.
             However, fully public onchain interactions create new risks exposing
             end users, solvers, AI agents, market makers, and other participants
             to exploitative tactics that degrade execution quality.
           </p>
-          <p className="font-medium text-lg md:text-xl mb-6 md:mb-12">
+          <p className="font-medium mb-6 md:mb-12 text-lg md:text-2xl">
             Fairblock leverages dynamic confidential computing to mitigate
             centralized risks and prevent information leakage in decentralized
-            applications. This unlocks Credible and Capital-Efficient DeFi
-            mechanisms and AI models.
+            applications. This unlocks Credible and Confidential DeFi mechanisms
+            and AI models.
           </p>
-          <button className="border border-black dark:border-white bg-orange-300 font-medium px-6 py-2 rounded-3xl text-black w-fit min-w-48">
-            <Link to={"/how-it-works"}>Try FairyRing</Link>
+          <button className="bg-fuchsia-300 hover:bg-fuchsia-400 border border-black/50 dark:border-white/50 font-marlide font-medium px-6 py-2 rounded-3xl text-black w-fit min-w-48">
+            <a href="https://www.fairblock.network/how-it-works">
+              Try FairyRing
+            </a>
           </button>
         </div>
       </div>
-    </div>
     </div>
   );
 };
