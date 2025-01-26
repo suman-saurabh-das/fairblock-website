8a7195e7f608f3dc5a931278128fcb72e5d536d3
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-01-26 13:07:40 +0530

    Updated Hero section - Added video bg & dark mode bg-color changed to black

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "../App";
 import { MasterSvg } from "../assets/svg/MasterSvg";
 import { TfiAngleDoubleDown } from "react-icons/tfi";
 import ScrambleText from "./ScrambleText";
@@ -5,31 +7,66 @@ import ScrambleText from "./ScrambleText";
 const scrollToSection = (sectionId: string) => {
   const section = document.getElementById(sectionId);
   if (section) {
-    section.scrollIntoView({ behavior: "smooth", block: "end" });
+    section.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 };
 
 const Hero = () => {
+  const { darkMode } = useTheme();
+  const [bgVideoURL, setBgVideoUrl] = useState<string>(
+    "/src/assets/video/Hero-bg-light.mp4"
+  );
+
+  useEffect(() => {
+    darkMode
+      ? setBgVideoUrl("/src/assets/video/Hero-bg-dark.mp4")
+      : setBgVideoUrl("/src/assets/video/Hero-bg-light.mp4");
+  }, [darkMode]);
+
   return (
-    <div className="flex flex-col items-center px-6 py-12 md:py-16">
-      <div className="">
-        <MasterSvg className="fill-black dark:fill-white h-96 w-full" />
-      </div>
-      <div className="mt-6 lg:mt-12">
-        <h1 className="font-marlide font-normal tracking-wider text-center text-3xl lg:text-4xl max-w-72 sm:max-w-fit">
-        <ScrambleText text="Build Farther with Web 3's Confidentiality Station" speed={50} scrambleOnLoad={true} />
-        </h1>
-      </div>
-      <div className="mt-8">
-        <button
-          className="flex flex-col gap-1 items-center"
-          onClick={() => scrollToSection("about")}
-        >
-          <TfiAngleDoubleDown className="animate-bounce duration-100 hover:bg-black/10 dark:hover:bg-black/50 p-2 rounded-full text-4xl" />
-        </button>
+    <div className="flex items-center justify-center min-h-[92vh] max-h-fit overflow-hidden relative mx-auto w-full max-w-screen-2xl">
+      {/* VIDEO BACKGROUND */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        key={bgVideoURL} // Forces video reload on src change
+        src={bgVideoURL}
+        className="absolute invert-0 inset-0 object-cover h-full w-full"
+      ></video>
+
+      {/* CONTENT OVERLAY */}
+      <div className="bg-[#58BDF6]/90 dark:bg-black/60 flex flex-col items-center justify-center  p-6 relative min-h-[92vh] max-h-fit w-full z-[5]">
+        <div className="flex flex-col items-center">
+          {/* HERO IMAGE CONTAINER */}
+          <div>
+            <MasterSvg className="fill-black dark:fill-white h-[22rem] w-full" />
+          </div>
+
+          {/* HERO TEXT CONTAINER */}
+          <div className="mt-6 lg:mt-12">
+            <h1 className="font-marlide font-normal tracking-wider text-center text-3xl lg:text-5xl max-w-72 sm:max-w-fit">
+              <ScrambleText
+                text="Build Farther with Web 3's Confidentiality Station"
+                speed={50}
+                scrambleOnLoad={true}
+              />
+            </h1>
+          </div>
+
+          {/* SCROLL DOWN BUTTON */}
+          <div className="mt-8">
+            <button
+              className="flex flex-col gap-1 items-center"
+              onClick={() => scrollToSection("about")}
+            >
+              <TfiAngleDoubleDown className="animate-bounce duration-100 hover:bg-black/10 dark:hover:bg-white/30 p-2 rounded-full text-4xl" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
-
 export default Hero;
