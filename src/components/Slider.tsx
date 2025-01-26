83e446ebd6dfedfee7894704cef0bb590e146a98
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-01-26 21:34:11 +0530

    Updated Sliding cards - Adjusted speed and changed size for mobile

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -26,7 +26,6 @@ type SliderProps = {
 
 const Slider: React.FC<SliderProps> = ({
   cards,
-  images,
   imageWithLinks,
   width,
   height,
@@ -46,22 +45,6 @@ const Slider: React.FC<SliderProps> = ({
       data-reverse={reverse}
     >
       <div className="list flex relative">
-        {/* Render images when images array is passed as props */}
-        {images &&
-          images.map((src, index) => (
-            <div
-              key={index}
-              className="item absolute"
-              style={
-                {
-                  "--position": `${index + 1}`,
-                } as React.CSSProperties
-              }
-            >
-              <img src={src} alt={`Slider ${index + 1}`} className="w-full" />
-            </div>
-          ))}
-
         {/* Render images with links when imageWithLinks array is passed as props */}
         {imageWithLinks &&
           imageWithLinks.map((item, index) => (
@@ -78,9 +61,11 @@ const Slider: React.FC<SliderProps> = ({
                 <img
                   src={item.imageUrl}
                   alt={item.name}
-                  className={`${item.isBlack && "dark:invert"} aspect-square object-contain p-4 w-full`}
+                  className={`${
+                    item.isBlack && "dark:invert"
+                  } aspect-square object-contain p-4 w-full`}
                 />
-                <p className="text-center">{item.name}</p>
+                <p className="text-base md:text-lg text-center">{item.name}</p>
               </a>
             </div>
           ))}
@@ -97,8 +82,10 @@ const Slider: React.FC<SliderProps> = ({
                 } as React.CSSProperties
               }
             >
-              <div className="border border-black dark:border-white flex flex-col gap-4 h-full items-center justify-around px-6 py-4 rounded-2xl text-center">
-                <h4 className="font-marlide text-4xl">{item.name}</h4>
+              <div className="border dark:bg-neutral-800 border-black/50 dark:border-white/50 flex flex-col gap-4 h-[99%] items-center justify-around px-6 py-4 rounded-2xl shadow-md shadow-black/20 dark:shadow-black text-center">
+                <h4 className="font-marlide text-3xl sm:text-4xl">
+                  {item.name}
+                </h4>
                 <img
                   src={item.imageUrl}
                   alt={`Slider ${index + 1}`}
--- src/components/SlidingCards.tsx
+++ src/components/SlidingCards.tsx
@@ -1,6 +1,7 @@
+import { useState, useEffect } from "react";
 import Slider from "./Slider";
 
-const cardsArray = [
+const SLIDING_CARDS_ARRAY = [
   {
     name: "Accessible",
     imageUrl: "/src/assets/images/Accessible.png",
@@ -29,15 +30,40 @@ const cardsArray = [
 ];
 
 const SlidingCards = () => {
+  const [dimensions, setDimensions] = useState({ width: 300, height: 410 });
+
+  useEffect(() => {
+    const updateDimensions = () => {
+      if (window.innerWidth < 769) {
+        // Mobile
+        setDimensions({ width: 240, height: 370 });
+      } else {
+        // Laptop/Desktop
+        setDimensions({ width: 300, height: 410 });
+      }
+    };
+
+    // Initial setup
+    updateDimensions();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", updateDimensions);
+
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener("resize", updateDimensions);
+    };
+  }, []);
+
   return (
-      <div className="px-6 py-12 md:py-20">
-        <div className="max-w-screen-xl mx-auto">
-          <Slider
-            cards={cardsArray}
-            width={300}
-            height={410}
-            quantity={cardsArray.length}
-          />
+    <div className="px-6 py-12 md:py-20">
+      <div className="max-w-screen-xl mx-auto">
+        <Slider
+          cards={SLIDING_CARDS_ARRAY}
+          width={dimensions.width}
+          height={dimensions.height}
+          quantity={SLIDING_CARDS_ARRAY.length}
+        />
       </div>
     </div>
   );
