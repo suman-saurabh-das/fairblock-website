f69bdd816b594d04cd8cfc1d1e309d2d28634616
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-02-05 17:44:21 +0530

    Added utils folder and files

---
+++ src/utils/images.ts
@@ -0,0 +1,27 @@
+export const CHARACTER_BLUE_KEY_IMAGE = "https://i.imgur.com/yQlNPuF.png";
+export const CHARACTER_BLUE_LANTERN_IMAGE = "https://i.imgur.com/nV428KM.png";
+export const CHARACTER_BLUE_LOCK_IMAGE = "https://i.imgur.com/sfTterI.png";
+export const CHARACTER_BLUE_SCROLL_IMAGE = "https://i.imgur.com/LvnaZ0b.png";
+export const CHARACTER_BLUE_SHIELD_IMAGE = "https://i.imgur.com/zSGZ6EG.png";
+
+export const CHARACTER_GREEN_KEY_IMAGE = "https://i.imgur.com/20Vc0RG.png";
+export const CHARACTER_GREEN_LANTERN_IMAGE = "https://i.imgur.com/hYaxN60.png";
+export const CHARACTER_GREEN_LOCK_IMAGE = "https://i.imgur.com/arZLEe4.png";
+export const CHARACTER_GREEN_SCROLL_IMAGE = "https://i.imgur.com/yJQb68O.png";
+export const CHARACTER_GREEN_SHIELD_IMAGE = "https://i.imgur.com/EY3BzFi.png";
+
+export const CHARACTER_ORANGE_KEY_IMAGE = "https://i.imgur.com/L5NuZGR.png";
+export const CHARACTER_ORANGE_LANTERN_IMAGE = "https://i.imgur.com/EWoFOnj.png";
+export const CHARACTER_ORANGE_LOCK_IMAGE = "https://i.imgur.com/r3JWQCY.png";
+export const CHARACTER_ORANGE_SCROLL_IMAGE = "https://i.imgur.com/6g9tKBK.png";
+export const CHARACTER_ORANGE_SHIELD_IMAGE = "https://i.imgur.com/LlEfJNy.png";
+
+export const CHARACTER_PURPLE_KEY_IMAGE = "https://i.imgur.com/SxPnOI2.png";
+export const CHARACTER_PURPLE_LANTERN_IMAGE = "https://i.imgur.com/j0cCLCN.png";
+export const CHARACTER_PURPLE_LOCK_IMAGE = "https://i.imgur.com/PpGFvkS.png";
+export const CHARACTER_PURPLE_SCROLL_IMAGE = "https://i.imgur.com/PC5jZog.png";
+export const CHARACTER_PURPLE_SHIELD_IMAGE = "https://i.imgur.com/fp1qFg7.png";
+
+export const FAIR_BLOCK_IMAGE = "https://i.imgur.com/jU7B6iq.png";
+export const HOW_IT_WORKS_LIGHT_IMAGE = "https://i.imgur.com/csDZHyR.png";
+export const HOW_IT_WORKS_DARK_IMAGE = "https://i.imgur.com/KIp160B.png";
---
+++ src/utils/links.ts
@@ -0,0 +1,2 @@
+export const DOCS_LINK = "https://docs.fairblock.network/docs/welcome/Vision";
+export const DOCS_C_APP_LINK = "https://docs.fairblock.network/docs/start-a-capp-in-5-minutes/";
